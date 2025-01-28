1cd6af0fd6a7fa5663e8df11aa72826525a6c5d5
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-28 15:02:55 -0500

    initial commit

---
+++ src/test/java/com/backendchallenge/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,14 @@
+package com.backendchallenge.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// This test is empty because it is only used to check if the context loads
+	}
+
+}
