ca103fa2813ee8e410feb64032ca0fa31f399002
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-29 07:12:49 -0500

    implement to  infrastructure layer unit test

--- src/main/java/com/backendchallenge/userservice/domain/until/ConstTest.java
+++ src/main/java/com/backendchallenge/userservice/domain/until/ConstTest.java
@@ -6,7 +6,6 @@ public class ConstTest {
     public static final String ROLE_NAME_TEST = "RoleTest";
     public static final Long ROLE_ID_TEST = 1L;
 
-    public static final Long USER_ID_TEST = 1L;
     public static final String EMAIL_VALID = "test@email.com";
     public static final String PASSWORD_VALID = "password123";
     public static final String DOCUMENT_VALID = "12345678";
@@ -16,7 +15,6 @@ public class ConstTest {
     public static final String NAME_VALID = "John";
     public static final String LAST_NAME_VALID = "Doe";
 
-    public static final Long USER_ID_NEW = 2L;
     public static final String EMAIL_NEW = "newemail@email.com";
     public static final String PASSWORD_NEW = "newpassword";
     public static final String DOCUMENT_NEW = "87654321";
@@ -30,7 +28,6 @@ public class ConstTest {
     public static final String INVALID_EMAIL_TEST = "INVALID_EMAIL";
     public static final String EMPTY_STRING = "";
 
-    public static final Long EXPECTED_USER_ID = 1L;
     public static final Long ROLE_ID_NEW = 2L;
     public static final String ROLE_NAME_NEW = "New Role";
     public static final String ENCODED_PASSWORD_TEST = "ENCODED";
