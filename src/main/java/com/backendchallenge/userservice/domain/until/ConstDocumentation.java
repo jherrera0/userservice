63a3b15629f8f4b3b0cb0ebb2599486ed91e90f8
Jose Herrera <jose.herrera@pragma.com.co>
2025-01-29 13:56:51 -0500

    add documentation

--- src/main/java/com/backendchallenge/userservice/domain/until/ConstDocumentation.java
+++ src/main/java/com/backendchallenge/userservice/domain/until/ConstDocumentation.java
@@ -8,6 +8,8 @@ public class ConstDocumentation {
     public static final String CODE_400 = "400";
     public static final String CREATE_OWNER_CODE_201 = "Owner user created successfully";
     public static final String CREATE_OWNER_CODE_400 = "Invalid data to request create owner user";
+    public static final String FIND_OWNER_OPERATION = "Find a owner user by id";
+    public static final String FIND_OWNER_CODE_201 = "Owner found process did successfully";
 
     private ConstDocumentation() {
     }
